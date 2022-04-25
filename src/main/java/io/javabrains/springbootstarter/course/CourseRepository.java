575b256d91ece0bdd577f49bcdeed2323fc8837c
J.Perez <leojjosepf@gmail.com>
2022-04-25 18:25:25 -0500

    JPA

---
+++ src/main/java/io/javabrains/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,11 @@
+package io.javabrains.springbootstarter.course;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+import io.javabrains.springbootstarter.topic.Topic;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+	//public List<Course> findByName(String name);
+	public List<Course> findByTopicId(String topicId);
+}
